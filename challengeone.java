70a05e9abb34f589af52c5f856650bf7321378df
Karey06 <joyphenomenal@gmail.com>
2025-04-29 16:43:46 +0300

    Greeting completed

--- challengeone.java
+++ challengeone.java
@@ -1,9 +1,9 @@
 public class challengeone {
-        void greeting(){
-            System.out.println("Hello there!");
-        }
-        public static void main(String[]args)
-    {
+    void greeting() {
+        System.out.println("Hello there!");
+    }
+
+    public static void main(String[] args) {
         challengeone greeting = new challengeone();
         greeting.greeting();
     }
