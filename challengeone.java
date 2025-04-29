03dcb1adb448a6fae2be5d9a4eaaa31a7b418281
Karey06 <joyphenomenal@gmail.com>
2025-04-29 16:23:04 +0300

    Challenge one completed

---
+++ challengeone.java
@@ -0,0 +1,10 @@
+public class challengeone {
+        void greeting(){
+            System.out.println("Hello there!");
+        }
+        public static void main(String[]args)
+    {
+        challengeone greeting = new challengeone();
+        greeting.greeting();
+    }
+}
